d18e3fa533d14f1a2ce47f06e749734738595605
JakeWong462 <wongjak000@alpinesd.org>
2025-09-23 21:29:46 -0600

    changes in script.js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const btn = document.querySelector('btn');
 btn.adddEventListener('click', sumbitName )
 function sumbitName()
 const btnHide = document.querySelector('#hide')
+hamburger.addEventListener('click',() => {
+    if (menuOpen == false) {
+        navLinks.style.display = "block";
+        menuOpen = true;
+    }
+    else if (menuOpen == true) {
+        navLinks.style.display = "none";
+        menuOpen = false;
+    }
+});

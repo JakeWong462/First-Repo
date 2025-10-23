24191d049725f73c5bc86a9e49420361017a07e5
Jake Hoi Chun Wong <wongjak000@alpinesd.org>
2025-10-23 08:54:03 -0600

    testing

--- nav.js
+++ nav.js
@@ -1,6 +1,5 @@
 const hamburger = document.querySelector(".hamburger");
 const burgerMenu = document.querySelector(".burger-menu");
-
 hamburger.addEventListener("click", () => {
   burgerMenu.classList.toggle("show-menu");
 });
--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const btn = document.querySelector('button');
-btn.addEventListener('click', submitName)
-function submitName()
-const btnHide = document.querySelector('hidebutton')
+const btnSubmit = document.getElementById("btnSubmit");
+const pText = document.getElementById("pText");
+const inputText = document.querySelector("#inputText");
+
+btnSubmit.addEventListener("click", displayText);
+
+function displayText() {
+  const inputValue = inputText.value;
+  console.log(inputValue);
+
+  pText.style.color = "red";
+
+  pText.style.fontWeight = "bold";
+
+  pText.innerHTML = inputValue;
+}

77e6b036ac338280a2056f95ed3e3f038c991a02
Jake Hoi Chun Wong <wongjak000@alpinesd.org>
2025-11-04 09:08:38 -0700

    Game

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ function displayText() {
 
   pText.innerHTML = inputValue;
 }
+
+/* let name = prompt("What's your name?");
+alert(`Hi ${name} how are you?`); 
+
+let color = prompt("Whats your favorite color?");
+if (color === "blue") {
+  alert(`Oh nice that's my favorite color too!`);
+} else {
+  alert(`That's a nice color!`);
+}*/
+
+const numberInp = document.quearySelector("inputSubmit");
+const guessBtn = document.quearySelector("guessBtn");
+const attemptConst = document.quearySelector("#attemptSpan");
+const youWon = document.quearySelector("youWon");
+let secretNumber = Math.floor(Math.random() * 10) + 1;
+guessBtn.addEventListener("click");
+function onGuess() {
+  attemptConst++;
+  const inputValue = Number(numberInp.value);
+}
+if (secretNumber === inputValue) {
+}
